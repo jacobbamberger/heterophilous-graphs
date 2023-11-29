c858914a4c0794b8b29d5321708ed803c32251f0
Jacob Bamberger <jacger@CSL15899.local>
2023-11-29 18:04:45 +0000

    fix sage bug

--- modules.py
+++ modules.py
@@ -163,16 +163,15 @@ class BDLSAGEModule(nn.Module):
         h = torch.einsum('a, acb -> cb', attention, different_times)
 
         # Importantly, the feedforward is before pulling back
+        h = torch.cat([x, h], axis=1)  # SAGE part
         h = self.feed_forward_module(graph, h)
 
         # Then we pull back
         vector_field = h.reshape(num_nodes, num_bundles, self.bundle_dim, -1)
         vector_field = torch.einsum('abcd, abde -> abce', node_rep.transpose(2, 3),
                                     vector_field)  # inverse is transpose
-        message = vector_field.reshape(num_nodes, self.dim)
-
-        x = torch.cat([x, message], axis=1)
-        return x
+        h = vector_field.reshape(num_nodes, self.dim)
+        return h
 
 def _check_dim_and_num_heads_consistency(dim, num_heads):
     if dim % num_heads != 0:
